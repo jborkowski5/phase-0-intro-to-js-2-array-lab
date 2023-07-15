8fd25225e47d1a7a3849b0fc59facdd4a45fec1a
Joseph <borkowski.joseph@gmail.com>
2023-07-14 20:33:46 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  
+  module.exports = {
+    cats,
+    destructivelyAppendCat
+  };
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  module.exports = {
+    cats,
+    destructivelyPrependCat
+  };
+
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  module.exports = {
+    cats,
+    destructivelyRemoveLastCat
+  };
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  module.exports = {
+    cats,
+    destructivelyRemoveFirstCat
+  };
+
+  function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+  }
+  
+  module.exports = {
+    cats,
+    appendCat
+  };
+
+  function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+  }
+  
+  module.exports = {
+    cats,
+    prependCat
+  };
+
+  function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+  }
+  
+  module.exports = {
+    cats,
+    removeLastCat
+  };
+
+  function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+  }
+  
+  module.exports = {
+    cats,
+    removeFirstCat
+  };
+  
